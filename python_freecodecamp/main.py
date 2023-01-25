45db82ea48cfce9073f2f823ced13666fa22c6a8
Berk Can <berkcan.ulubas@gmx.de>
2023-01-26 00:42:17 +0100

    _

--- python_freecodecamp/main.py
+++ python_freecodecamp/main.py
@@ -1,3 +1,19 @@
 def get_choices():
     player_choice = "rock"
     computer_choice = "paper"
+    return computer_choice
+
+choices = get_choices()
+print(choices)
+
+dict = {"name": "beau", "color": "blue"}
+
+
+
+'''
+def greeting():
+    return "Hi"
+
+response = greeting()
+print(response)
+'''
