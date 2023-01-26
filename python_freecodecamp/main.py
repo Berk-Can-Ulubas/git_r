fa41ba5c257afa46c9c00fd29b665ba7386ed412
Berk Can <berkcan.ulubas@gmx.de>
2023-01-26 18:55:54 +0100

    _

--- python_freecodecamp/main.py
+++ python_freecodecamp/main.py
@@ -1,7 +1,7 @@
 import random
 
 def get_choices():
-    player_choice = input("Enter a choice (rock, paper, scissors: ")
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
     options = ["rock", "paper", "scissors"]
     computer_choice = random.choice(options)
     choices = {"player": player_choice, "computer": computer_choice}
@@ -30,7 +30,8 @@ def check_win(player, computer):
 
 
 choices = get_choices()
-
+result = check_win(choices["player"], choices["computer"])
+print(result)
 
 #dict = {"name": "beau", "color": choices}
 
