dd4551051b12556af1c0cb60efc3d4e7fc84e9ff
Berk Can <berkcan.ulubas@gmx.de>
2023-01-26 10:24:02 +0100

    _

--- python_freecodecamp/main.py
+++ python_freecodecamp/main.py
@@ -1,19 +1,39 @@
+import random
+
 def get_choices():
-    player_choice = "rock"
-    computer_choice = "paper"
-    return computer_choice
+    player_choice = input("Enter a choice (rock, paper, scissors: ")
+    options = ["rock", "paper", "scissor"]
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
 
-choices = get_choices()
-print(choices)
+def check_win(player, computer):
+    print(f"You chose {player}.")
+    print(f"Computer chose {computer}.")
+    if player == computer:
+        return "its a tie!"
 
-dict = {"name": "beau", "color": "blue"}
 
 
+check_win("rock", "rock")
 
-'''
-def greeting():
-    return "Hi"
+#dict = {"name": "beau", "color": choices
 
-response = greeting()
-print(response)
-'''
+#choices = get_choices()
+#print(choices)
+
+
+#def greeting():
+#    return "Hi"
+
+#response = greeting()
+#print(response)
+
+
+#food = ["pizza", "carrots", "eggs"]
+#dinner = random.choice(food)
+
+#a = 3
+#b = 5 
+#if a < b:
+#    print("yes")
